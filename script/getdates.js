88a43ae5c6d1586364afd94f6b4646e7242491ce
clarkedav <esu23002@byui.edu>
2024-06-29 14:41:39 -0700

    555

--- script/getdates.js
+++ script/getdates.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Set the current year dynamically
-    document.getElementById('currentyear').textContent = new Date().getFullYear();
+    document.getElementById('2024').textContent = new Date().getFullYear();
 
     // Set the last modified date dynamically
     document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
