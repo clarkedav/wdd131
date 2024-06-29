ffbc6857e72b7276528298ef396685dfd463a988
clarkedav <esu23002@byui.edu>
2024-06-29 00:38:00 -0700

    33333

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Set the current year dynamically
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+    // Set the last modified date dynamically
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+});
